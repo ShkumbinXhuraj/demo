aed124d42bb29ec7a2c454a4166fa4cae03f653a
ShkumbinXhuraj <692833+ShkumbinXhuraj@users.noreply.github.com>
2017-08-03 01:56:40 -1200

    Initial commit

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.apache.commons.lang3.ObjectUtils;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        ObjectUtils.allNotNull(null);
+    }
+
+}
